0e4896de3f8e5722be465311edefecb2f836cc2d
Can Berkol <can.berkol@biberltd.com>
2016-04-15 12:23:15 +0300

    getters setters added.

--- Entity/Package.php
+++ Entity/Package.php
@@ -1,11 +1,18 @@
 <?php
+/**
+ * @author		Can Berkol
+ * @author		Özge Deniz Uysal
+ *
+ * @copyright   Biber Ltd. (http://www.biberltd.com) (C) 2015
+ * @license     GPLv3
+ */
 use Doctrine\ORM\Mapping AS ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="package", options={"charset":"utf8","collate":"utf8_turkish_ci","engine":"innodb"})
  */
-class Package
+class SubscriptionPackage
 {
     /**
      * @ORM\Id
@@ -28,4 +35,124 @@ class Package
      * @ORM\Column(type="text", nullable=false)
      */
     private $type;
+}
+
+/**
+ * 
+ * 
+ */
+class Package extends \BiberLtd\Bundle\CoreBundle\CoreEntity
+{
+    /**
+     * 
+     * 
+     * 
+     * @var int
+     */
+    private $id;
+
+    /**
+     * 
+     * @var string
+     */
+    private $code;
+
+    /**
+     * 
+     * @var float
+     */
+    private $fee;
+
+    /**
+     * 
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @return int
+     */
+    public function getId(){
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId(int $id){
+        if(!$this->setModified('id', $id)->isModified()){
+            return $this;
+        }
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(){
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return $this
+     */
+    public function setCode(string $code){
+        if(!$this->setModified('code', $code)->isModified()){
+            return $this;
+        }
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getFee(){
+        return $this->fee;
+    }
+
+    /**
+     * @param float $fee
+     *
+     * @return $this
+     */
+    public function setFee(float $fee){
+        if(!$this->setModified('fee', $fee)->isModified()){
+            return $this;
+        }
+        $this->fee = $fee;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(){
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return $this
+     */
+    public function setType(string $type){
+        if(!$this->setModified('type', $type)->isModified()){
+            return $this;
+        }
+        $this->type = $type;
+
+        return $this;
+    }
+
+
+
 }
--- Entity/Subscription.php
+++ Entity/Subscription.php
@@ -11,56 +11,300 @@ class Subscription
      * @ORM\Id
      * @ORM\Column(type="bigint", length=20, options={"unsigned":true})
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
      */
     private $id;
     /**
      * @ORM\Column(type="decimal", nullable=true)
+     * @var float
      */
     private $price;
 
     /**
      * @ORM\Column(type="date", nullable=false)
+     * @var \DateTime
      */
     private $date_start;
 
     /**
      * @ORM\Column(type="date", nullable=true)
+     * @var \DateTime
      */
     private $date_end;
 
     /**
      * @ORM\Column(type="date", nullable=true)
+     * @var \DateTime
      */
     private $date_cancel;
 
     /**
      * @ORM\Column(type="text", nullable=false)
+     * @var string
      */
     private $status;
 
     /**
-     * @ORM\Column(type="text", nullable=false)
+     * @ORM\ManyToOne(targetEntity="SubscriptionPackage")
+     * @ORM\JoinColumn(name="package", referencedColumnName="id", onDelete="CASCADE")
+     * @var SubscriptionPackage
      */
     private $package;
 
     /**
      * @ORM\Column(type="text", nullable=false)
+     * @var string
      */
     private $promotion;
 
     /**
      * @ORM\Column(type="text", nullable=false)
+     * @var string
      */
     private $payment_status;
 
     /**
-     * @ORM\Column(type="text", nullable=false)
+     * @ORM\Column(type="decimal", nullable=false)
+     * @var float
      */
-    private $remain_amount;
+    private $remaining_amount;
 
     /**
      * @ORM\ManyToOne(targetEntity="BiberLtd\Bundle\MemberManagementBundle\Entity\Member")
      * @ORM\JoinColumn(name="member", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     * @var \BiberLtd\Bundle\MemberManagementBundle\Entity\Member
      */
     private $member;
+
+    /**
+     * @return int
+     */
+    public function getId(){
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId(int $id){
+        if(!$this->setModified('id', $id)->isModified()){
+            return $this;
+        }
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(){
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     *
+     * @return $this
+     */
+    public function setPrice(float $price){
+        if(!$this->setModified('price', $price)->isModified()){
+            return $this;
+        }
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateStart(){
+        return $this->date_start;
+    }
+
+    /**
+     * @param \DateTime $date_start
+     *
+     * @return $this
+     */
+    public function setDateStart(\DateTime $date_start){
+        if(!$this->setModified('date_start', $date_start)->isModified()){
+            return $this;
+        }
+        $this->date_start = $date_start;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateEnd(){
+        return $this->date_end;
+    }
+
+    /**
+     * @param \DateTime $date_end
+     *
+     * @return $this
+     */
+    public function setDateEnd(\DateTime $date_end){
+        if(!$this->setModified('date_end', $date_end)->isModified()){
+            return $this;
+        }
+        $this->date_end = $date_end;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateCancel(){
+        return $this->date_cancel;
+    }
+
+    /**
+     * @param \DateTime $date_cancel
+     *
+     * @return $this
+     */
+    public function setDateCancel(\DateTime $date_cancel){
+        if(!$this->setModified('date_cancel', $date_cancel)->isModified()){
+            return $this;
+        }
+        $this->date_cancel = $date_cancel;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(){
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     *
+     * @return $this
+     */
+    public function setStatus(string $status){
+        if(!$this->setModified('status', $status)->isModified()){
+            return $this;
+        }
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return \SubscriptionPackage
+     */
+    public function getPackage(){
+        return $this->package;
+    }
+
+    /**
+     * @param \SubscriptionPackage $package
+     *
+     * @return $this
+     */
+    public function setPackage(SubscriptionPackage $package){
+        if(!$this->setModified('package', $package)->isModified()){
+            return $this;
+        }
+        $this->package = $package;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPromotion(){
+        return $this->promotion;
+    }
+
+    /**
+     * @param string $promotion
+     *
+     * @return $this
+     */
+    public function setPromotion(string $promotion){
+        if(!$this->setModified('promotion', $promotion)->isModified()){
+            return $this;
+        }
+        $this->promotion = $promotion;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPaymentStatus(){
+        return $this->payment_status;
+    }
+
+    /**
+     * @param string $payment_status
+     *
+     * @return $this
+     */
+    public function setPaymentStatus(string $payment_status){
+        if(!$this->setModified('payment_status', $payment_status)->isModified()){
+            return $this;
+        }
+        $this->payment_status = $payment_status;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRemainingAmount(){
+        return $this->remaining_amount;
+    }
+
+    /**
+     * @param float $remaining_amount
+     *
+     * @return $this
+     */
+    public function setRemainingAmount(float $remaining_amount){
+        if(!$this->setModified('remaining_amount', $remaining_amount)->isModified()){
+            return $this;
+        }
+        $this->remaining_amount = $remaining_amount;
+
+        return $this;
+    }
+
+    /**
+     * @return \BiberLtd\Bundle\MemberManagementBundle\Entity\Member
+     */
+    public function getMember(){
+        return $this->member;
+    }
+
+    /**
+     * @param \BiberLtd\Bundle\MemberManagementBundle\Entity\Member $member
+     *
+     * @return $this
+     */
+    public function setMember(\BiberLtd\Bundle\MemberManagementBundle\Entity\Member $member){
+        if(!$this->setModified('member', $member)->isModified()){
+            return $this;
+        }
+        $this->member = $member;
+
+        return $this;
+    }
+
 }
